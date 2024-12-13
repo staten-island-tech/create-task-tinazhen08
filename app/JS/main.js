aff51b5187689e831b1dd0062ee1b18867a05c31
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-12-13 14:00:05 -0500

    Update main.js

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,1 +1,35 @@
 import "../css/style.css";
+
+async function getData() {
+  try {
+    //returns a promise
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=50&type=multiple"
+    );
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //convert promise to json
+      const data = await response.json();
+      console.log(data);
+      return data.results;
+    }
+  } catch (error) {
+    alert("hey  I could not find that agent");
+  }
+}
+
+getData();
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+async function questiion() {
+  const data = getData();
+}
