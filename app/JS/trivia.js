80f3c3e2cedb04430dff07d772ef78ba2dced310
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2025-01-13 00:23:00 -0500

    Update trivia.js

--- app/JS/trivia.js
+++ app/JS/trivia.js
@@ -502,4 +502,342 @@ const books = {
     ],
   };
 
-  export {books, computers, generalKnowledge, mathematics, music, mythology, television, videoGames}
+  const boardGames = {
+    results: [
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Board Games",
+        question: "When was the board game Twister, released to the public?",
+        correct_answer: "April 1966",
+        incorrect_answers: ["September 1965", "January 1969", "February 1966"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Board Games",
+        question:
+          "How many pieces are there on the board at the start of a game of chess?",
+        correct_answer: "32",
+        incorrect_answers: ["16", "20", "36"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Board Games",
+        question:
+          "In board games, an additional or ammended rule that applies to a certain group or place is informally known as a &quot;what&quot; rule?",
+        correct_answer: "House",
+        incorrect_answers: ["Custom", "Extra", "Change"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Board Games",
+        question: "How many dice are used in the game of Yahtzee?",
+        correct_answer: "Five",
+        incorrect_answers: ["Four", "Six", "Eight"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Board Games",
+        question:
+          "The board game Monopoly takes its street names from which real American city?",
+        correct_answer: "Atlantic City, New Jersey",
+        incorrect_answers: [
+          "Las Vegas, Nevada",
+          "Duluth, Minnesota",
+          "Charleston, South Carolina",
+        ],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Board Games",
+        question:
+          "Europa Universalis is a strategy video game based on which French board game?",
+        correct_answer: "Europa Universalis",
+        incorrect_answers: [
+          "Europe and the Universe",
+          "Europa!",
+          "Power in Europe",
+        ],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Board Games",
+        question: "How many points is the Z tile worth in Scrabble?",
+        correct_answer: "10",
+        incorrect_answers: ["8", "5", "6"],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Board Games",
+        question:
+          "At the start of a standard game of the Monopoly, if you throw a double six, which square would you land on?",
+        correct_answer: "Electric Company",
+        incorrect_answers: ["Water Works", "Chance", "Community Chest"],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Board Games",
+        question:
+          "In what year was the card game Magic: the Gathering first introduced?",
+        correct_answer: "1993",
+        incorrect_answers: ["1987", "1998", "2003"],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Board Games",
+        question:
+          "In &quot;Magic: The Gathering&quot;, what instant card has the highest converted mana cost?",
+        correct_answer: "Blinkmoth Infusion",
+        incorrect_answers: [
+          "Vitalizing Wind",
+          " Chant of Vitu-Ghazi",
+          "Assert Authority",
+        ],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Board Games",
+        question:
+          "On a standard Monopoly board, how much do you have to pay for Tennessee Ave?",
+        correct_answer: "$180",
+        incorrect_answers: ["$200", "$160", "$220"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Board Games",
+        question:
+          "What was the development code name for the &quot;Urza&#039;s Destiny&quot; expansion for &quot;Magic: The Gathering&quot;, released in 1999?",
+        correct_answer: "Chimichanga",
+        incorrect_answers: ["Burrito", "Taquito", "Enchilada"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Board Games",
+        question:
+          "How many rooms are there, not including the hallways and the set of stairs, in the board game &quot;Clue&quot;?",
+        correct_answer: "9",
+        incorrect_answers: ["1", "6", "10"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Board Games",
+        question:
+          "What is the most challenging monster in the Dungeons &amp; Dragons 5th Edition Monster Manual?",
+        correct_answer: "Tarrasque",
+        incorrect_answers: ["Beholder", "Displacer Beast", "Lich"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Board Games",
+        question:
+          "In standard Monopoly, what&#039;s the rent if you land on Park Place with no houses?",
+        correct_answer: "$35",
+        incorrect_answers: ["$30", "$50", "$45"],
+      },
+    ],
+  };
+
+  const film = {
+    results: [
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Film",
+        question: "What was the first James Bond film?",
+        correct_answer: "Dr. No",
+        incorrect_answers: ["Goldfinger", "From Russia With Love", "Thunderball"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Film",
+        question:
+          "Which movie contains the quote, &quot;Say hello to my little friend!&quot;?",
+        correct_answer: "Scarface",
+        incorrect_answers: ["Reservoir Dogs", "Heat", "Goodfellas"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Film",
+        question:
+          "In the movie &quot;Blade Runner&quot;, what is the term used for human-like androids ?",
+        correct_answer: "Replicants",
+        incorrect_answers: ["Cylons", "Synthetics", "Skinjobs"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Film",
+        question:
+          "The 1996 film &#039;Fargo&#039; is primarily set in which US state?",
+        correct_answer: "Minnesota",
+        incorrect_answers: ["North Dakota", "South Dakota", "Wisconsin"],
+      },
+      {
+        type: "multiple",
+        difficulty: "easy",
+        category: "Entertainment: Film",
+        question:
+          "In the movie &quot;Cast Away&quot; the main protagonist&#039;s best friend while on the island is named",
+        correct_answer: "Wilson",
+        incorrect_answers: ["Carson", "Jackson", "Willy"],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Film",
+        question: "Johnny Depp made his big-screen acting debut in which film?",
+        correct_answer: "A Nightmare on Elm Street",
+        incorrect_answers: [
+          "My Bloody Valentine",
+          "Halloween",
+          "Friday the 13th",
+        ],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Film",
+        question:
+          "Which actor and martial artist starred as Colonel Guile in the 1994 action film adaptation of Street Fighter?",
+        correct_answer: "Jean-Claude Van Damme",
+        incorrect_answers: ["Chuck Norris", "Steven Seagal", "Scott Adkins"],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Film",
+        question:
+          "Who plays &quot;Bruce Wayne&quot; in the 2008 movie &quot;The Dark Knight&quot;?",
+        correct_answer: "Christian Bale",
+        incorrect_answers: ["Michael Caine", "Ron Dean", "Heath Ledger"],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Film",
+        question:
+          "In the 1979 British film &quot;Quadrophenia&quot; what is the name of the main protagonist?",
+        correct_answer: "Jimmy Cooper",
+        incorrect_answers: ["Pete Townshend", "Franc Roddam", "Archie Bunker"],
+      },
+      {
+        type: "multiple",
+        difficulty: "medium",
+        category: "Entertainment: Film",
+        question:
+          "Which 1958 movie starred Kirk Douglas and Tony Curtis as half-brothers Einar and Eric?",
+        correct_answer: "The Vikings ",
+        incorrect_answers: ["The Long Ships", "Prince Valiant", "Spartacus"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "In order to cut costs, what were most of the extras of Mad Max (1979) paid with?",
+        correct_answer: "Beer",
+        incorrect_answers: ["Fast food", "Food stamps", "They weren&#039;t paid"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question: "In the Friday The 13th series, what year did Jason drown in?",
+        correct_answer: "1957",
+        incorrect_answers: ["1955", "1953", "1959"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "The film Mad Max: Fury Road features the Dies Irae  from which composer&#039;s requiem?",
+        correct_answer: "Verdi",
+        incorrect_answers: ["Mozart", "Berlioz", "Brahms"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "In the &quot;Jurassic Park&quot; universe, when did &quot;Jurassic Park: San Diego&quot; begin construction?",
+        correct_answer: "1985",
+        incorrect_answers: ["1988", "1986", "1993"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "Which of the following films was directed by Ivan Reitman, written by Gary Ross, featured Kevin Kline, and was released in 1993?",
+        correct_answer: "Dave",
+        incorrect_answers: ["John", "Will", "Carl"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "What was the name of the actor who played Leatherface in the 1974 horror film, The Texas Chainsaw Massacre?",
+        correct_answer: "Gunnar Hansen",
+        incorrect_answers: ["Edwin Neal", "John Dugan", "Joe Bill Hogan"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "Which singer is portrayed by Bruce Campbell in the 2002 film &#039;Bubba Ho-Tep&#039;?",
+        correct_answer: "Elvis Presley",
+        incorrect_answers: ["Johnny Cash", "Hank Williams, Sr.", "Buddy Holly"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "Which 1994 film did Roger Ebert famously despise, saying &quot;I hated hated hated hated hated this movie&quot;.",
+        correct_answer: "North",
+        incorrect_answers: [
+          "3 Ninjas Kick Back",
+          "The Santa Clause",
+          "Richie Rich",
+        ],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question: "In Big Hero 6, what were Baymax&#039;s motions modeled after?",
+        correct_answer: "Baby penguins",
+        incorrect_answers: ["Baby monkeys", "Baby ostriches", "Baby bears"],
+      },
+      {
+        type: "multiple",
+        difficulty: "hard",
+        category: "Entertainment: Film",
+        question:
+          "How old was Laurence Fishburne when Francis Ford Coppola cast him in Apocalypse Now (1979)? ",
+        correct_answer: "14",
+        incorrect_answers: ["16", "18", "20"],
+      },
+    ],
+  };
+
+  export {books, computers, generalKnowledge, mathematics, music, mythology, television, videoGames, boardGames, film}
